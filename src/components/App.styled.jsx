fee5db3340eebf92dddb5f4f2636a86adac53530
YuriyBerketa <115158490+YuriyBerketa@users.noreply.github.com>
2023-05-10 16:25:45 +0300

    fixx

--- src/components/App.styled.jsx
+++ src/components/App.styled.jsx
@@ -4,7 +4,7 @@ export const Div = styled.div`
   padding-left: 15px;
   padding-right: 15px;
   height: 100vh;
-  width: 600px;
+  
   display: flex;
   flex-direction: column;
   align-items: center;
